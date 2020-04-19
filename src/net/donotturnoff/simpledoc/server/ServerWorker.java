70f0485ea44b35a4fc15cafe0096ac58cf0f0b07
donotturnoff <admin@donotturnoff.net>
2020-04-19 14:51:29 +0100

    Implemented halt

--- src/net/donotturnoff/simpledoc/server/ServerWorker.java
+++ src/net/donotturnoff/simpledoc/server/ServerWorker.java
@@ -2,8 +2,26 @@ package net.donotturnoff.simpledoc.server;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class ServerWorker implements Runnable {
+
+    private static final Logger logger = Logger.getLogger(SDTPServer.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("log%u.txt", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            fileHandler.setLevel(Level.INFO);
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to open log file", e);
+        }
+    }
+
     private Socket c;
     private BufferedReader in;
     private PrintWriter out;
@@ -20,6 +38,13 @@ public class ServerWorker implements Runnable {
     }
 
     void halt() {
-
+        try {
+            c.shutdownInput();
+            c.shutdownOutput();
+            c.close();
+            logger.log(Level.FINE, "Halted server worker");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to gracefully halt server worker", e);
+        }
     }
 }
