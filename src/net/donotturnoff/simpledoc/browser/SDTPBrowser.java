285f6767fec323f9fbb46cf42f7a6f0e5fdc8d4a
donotturnoff <admin@donotturnoff.net>
2020-04-27 18:50:48 +0100

    Created basic GUI layout for browser

---
+++ src/net/donotturnoff/simpledoc/browser/SDTPBrowser.java
@@ -0,0 +1,97 @@
+package net.donotturnoff.simpledoc.browser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SDTPBrowser implements ActionListener {
+
+    // Containers
+    private JFrame gui;
+    private JPanel navBar, statusBar;
+    private JTabbedPane tabbedPane;
+
+    // Menus
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+
+    // Inputs
+    private JButton backBtn, forwardsBtn, reloadBtn, newTabBtn;
+    private JTextField urlBar;
+
+    // Labels
+    private JLabel statusLabel;
+
+    public static void main(String[] args) {
+        SDTPBrowser browser = new SDTPBrowser();
+        browser.run();
+    }
+
+    private void run() {
+        SwingUtilities.invokeLater(this::createAndShowGUI);
+    }
+
+    private void createAndShowGUI() {
+        createWidgets();
+        configureWidgets();
+        constructGUI();
+        showGUI();
+    }
+
+    private void createWidgets() {
+        gui = new JFrame("Simpledoc browser v0.1");
+        navBar = new JPanel();
+        statusBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        tabbedPane = new JTabbedPane();
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+
+        backBtn = new JButton("\u2b60");
+        forwardsBtn = new JButton("\u2b62");
+        reloadBtn = new JButton("\u27f3");
+        newTabBtn = new JButton("+");
+        urlBar = new JTextField(60);
+
+        statusLabel = new JLabel("Welcome", JLabel.LEFT);
+    }
+
+    private void configureWidgets() {
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gui.setMinimumSize(new Dimension(800, 600));
+
+        backBtn.addActionListener(this);
+        forwardsBtn.addActionListener(this);
+        reloadBtn.addActionListener(this);
+        newTabBtn.addActionListener(this);
+    }
+
+    private void constructGUI() {
+        navBar.add(backBtn);
+        navBar.add(forwardsBtn);
+        navBar.add(reloadBtn);
+        navBar.add(urlBar);
+        navBar.add(newTabBtn);
+
+        menuBar.add(fileMenu);
+
+        statusBar.add(statusLabel);
+
+        Container pane = gui.getContentPane();
+        gui.setJMenuBar(menuBar);
+        pane.add("North", navBar);
+        pane.add("Center", tabbedPane);
+        pane.add("South", statusBar);
+    }
+
+    private void showGUI() {
+        gui.setVisible(true);
+        gui.setExtendedState(gui.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
