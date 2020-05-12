e65554c5c805e8455208198b58a3cc0a9d4ba94d
donotturnoff <admin@donotturnoff.net>
2020-05-12 18:20:14 +0100

    Fleshed out Element class and made abstract

--- src/net/donotturnoff/simpledoc/browser/Element.java
+++ src/net/donotturnoff/simpledoc/browser/Element.java
@@ -1,7 +1,56 @@
 package net.donotturnoff.simpledoc.browser;
 
 import java.util.List;
+import java.util.Map;
 
-class Element {
-    private List<Element> children;
+abstract class Element {
+    protected String name;
+    protected Map<String, String> attributes;
+    protected List<Element> children;
+
+    public List<Element> getChildren() {
+        return children;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return toString("");
+    }
+
+    private String toString(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent);
+        sb.append(name);
+        if (attributes.size() > 0) {
+            sb.append("(");
+            for (String k: attributes.keySet()) {
+                String v = attributes.get(k);
+                sb.append(k);
+                sb.append("=\"");
+                sb.append(v);
+                sb.append("\", ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(")");
+        }
+        if (children.size() > 0) {
+            sb.append(" {\n");
+            for (Element c: children) {
+                sb.append(indent);
+                sb.append(c.toString(indent+"\t"));
+                sb.append("\n");
+            }
+            sb.append(indent);
+            sb.append("}");
+        }
+        return sb.toString();
+    }
 }
