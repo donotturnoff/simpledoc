76e0b97b7eec5112a8dd5c44e7c1edbce627ce92
donotturnoff <admin@donotturnoff.net>
2020-07-21 17:13:54 +0100

    Remove comma token

--- src/net/donotturnoff/simpledoc/browser/lexing/TokenType.java
+++ src/net/donotturnoff/simpledoc/browser/lexing/TokenType.java
@@ -1,5 +1,5 @@
 package net.donotturnoff.simpledoc.browser.lexing;
 
 public enum TokenType {
-    EOF, LPAREN, RPAREN, LBRACE, RBRACE, EQUALS, COMMA, STRING, IDENT
+    EOF, LPAREN, RPAREN, LBRACE, RBRACE, EQUALS, STRING, IDENT
 }
