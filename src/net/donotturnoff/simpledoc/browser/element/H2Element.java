443363001286cb821d2313b749b991dc0736a4a8
donotturnoff <admin@donotturnoff.net>
2020-07-22 07:41:12 +0100

    Add h2 element

---
+++ src/net/donotturnoff/simpledoc/browser/element/H2Element.java
@@ -0,0 +1,24 @@
+package net.donotturnoff.simpledoc.browser.element;
+
+import net.donotturnoff.simpledoc.browser.Page;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class H2Element extends BoxElement {
+
+    public final static Map<String, String> defaultStyle = new HashMap<>();
+
+    static {
+        defaultStyle.put("font_family", Font.SERIF);
+        defaultStyle.put("font_size", "24");
+        defaultStyle.put("font_style", "bold");
+        defaultStyle.put("cursor", "text");
+    }
+
+    public H2Element(Page page, Map<String, String> attributes, List<Element> children) {
+        super(page, "h2", attributes, children);
+    }
+}
