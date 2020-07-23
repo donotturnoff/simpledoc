ed5bb02f0597a8837145d7edcb43d52a5a59c409
donotturnoff <admin@donotturnoff.net>
2020-07-23 10:45:22 +0100

    Explicitly specify inheritable styles
    Some styles should be inherited (e.g. font_family, colour) and others shouldn't (e.g. padding_*)

--- src/net/donotturnoff/simpledoc/browser/element/BodyElement.java
+++ src/net/donotturnoff/simpledoc/browser/element/BodyElement.java
@@ -10,6 +10,15 @@ import java.util.Map;
 
 public class BodyElement extends BoxElement {
 
+    public final static Map<String, String> defaultStyle = new HashMap<>();
+
+    static {
+        defaultStyle.put("padding_top", "5");
+        defaultStyle.put("padding_bottom", "5");
+        defaultStyle.put("padding_left", "5");
+        defaultStyle.put("padding_right", "5");
+    }
+
     public BodyElement(Page page, Map<String, String> attributes, List<Element> children) {
         super(page, "body", attributes, children);
     }
--- src/net/donotturnoff/simpledoc/browser/styling/SDMLStyler.java
+++ src/net/donotturnoff/simpledoc/browser/styling/SDMLStyler.java
@@ -4,15 +4,34 @@ import net.donotturnoff.simpledoc.browser.element.Element;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SDMLStyler {
+    private static final Set<String> inheritableStyles = new HashSet<>();
+
+    static {
+        inheritableStyles.add("font_family");
+        inheritableStyles.add("font_size");
+        inheritableStyles.add("font_style");
+        inheritableStyles.add("text_decoration");
+        inheritableStyles.add("colour");
+        inheritableStyles.add("background_colour");
+        inheritableStyles.add("cursor");
+    }
+
     public void style(Element root) {
         style(root, Map.of());
     }
     
     private void style(Element element, Map<String, String> parentStyle) {
-        Map<String, String> style = new HashMap<>(parentStyle);
+        Map<String, String> style = new HashMap<>();
+        for (String s: inheritableStyles) {
+            if (parentStyle.containsKey(s)) {
+                style.put(s, parentStyle.get(s));
+            }
+        }
         Map<String, String> defaultStyle = new HashMap<>();
         try {
             Field field = element.getClass().getField("defaultStyle");
