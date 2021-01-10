01f346c6f39234b0faa8daa093873db175bf52b4
donotturnoff <admin@donotturnoff.net>
2021-01-10 16:03:20 +0000

    Restrict title element to only have one child, a text element

--- src/net/donotturnoff/simpledoc/browser/element/TitleElement.java
+++ src/net/donotturnoff/simpledoc/browser/element/TitleElement.java
@@ -1,6 +1,7 @@
 package net.donotturnoff.simpledoc.browser.element;
 
 import net.donotturnoff.simpledoc.browser.Page;
+import net.donotturnoff.simpledoc.browser.sdml.SDMLException;
 
 import javax.swing.*;
 import java.util.List;
@@ -8,18 +9,20 @@ import java.util.Map;
 
 public class TitleElement extends Element {
 
-    public TitleElement(Page page, Map<String, String> attributes, List<Element> children) {
+    private final String text;
+
+    public TitleElement(Page page, Map<String, String> attributes, List<Element> children) throws SDMLException {
         super(page, "title", attributes, children);
+        Element c;
+        if (children.size() != 1 || !((c = children.get(0)) instanceof TextElement)) {
+            throw new SDMLException("Title element must contain a single text element as its child");
+        }
+        this.text = ((TextElement) c).getText();
     }
 
-    // TODO: fail if title element contains something other than a single text child
     @Override
     public void render(JPanel parentPanel) {
-        for (Element c: children) {
-            if (c instanceof TextElement) {
-                page.setTitle(((TextElement) c).getText());
-            }
-        }
+        page.setTitle(text);
     }
 
     @Override
